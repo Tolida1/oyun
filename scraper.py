5e5ec2cbb3c59e32cc4ad8a5b4c43eddff943b16
Tolida1 <105459096+Tolida1@users.noreply.github.com>
2026-03-04 22:53:09 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,37 +2,37 @@ import requests
 import xml.etree.ElementTree as ET
 import json
 
-FEED_URL = "https://dizipal.bar/dizi-kategori/aksiyon/feed/"
+FEED_URL = "https://dizipal.uk/dizi-kategori/aksiyon/feed/"
 
 headers = {
-    "User-Agent": "Mozilla/5.0"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+    "Accept": "application/rss+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Referer": "https://google.com",
+    "Connection": "keep-alive"
 }
 
 items = []
 
 try:
     r = requests.get(FEED_URL, headers=headers, timeout=15)
+    print("Status:", r.status_code)
     r.raise_for_status()
 
     root = ET.fromstring(r.content)
-
     channel = root.find("channel")
 
     for item in channel.findall("item"):
         title = item.find("title").text if item.find("title") is not None else ""
         link = item.find("link").text if item.find("link") is not None else ""
-        pubDate = item.find("pubDate").text if item.find("pubDate") is not None else ""
 
         items.append({
             "title": title,
-            "link": link,
-            "pubDate": pubDate
+            "link": link
         })
 
 except Exception as e:
     print("Hata:", e)
 
-# HER DURUMDA DOSYA OLUŞSUN
 with open("output.json", "w", encoding="utf-8") as f:
     json.dump(items, f, ensure_ascii=False, indent=4)
 
